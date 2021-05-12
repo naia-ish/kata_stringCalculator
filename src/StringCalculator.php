f93a693ede5f8279ab47baaf2abf25c2491f3c82
naiara <naiara@aircury.com>
2021-05-12 12:31:49 +0200

    add to method if string has two numbers

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -13,6 +13,10 @@ class StringCalculator
 
         if( 1 === strlen($string) ) {
             $sum = (int) $string;
+        } elseif ( 3 === strlen($string)) {
+            $number = explode(",", $string);
+            
+            $sum += (int) $number[0] + (int) $number[1];
         }
 
         return $sum;
